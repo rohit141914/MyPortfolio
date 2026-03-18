8608101eaac0907f41c10b0204bd6343f659a2b3
rohit141914 <rohitnainindian@gmail.com>
2026-03-19 01:30:43 +0530

    Added new portfolio images and removed position feild from the left container

--- src/components/ResumePanel.jsx
+++ src/components/ResumePanel.jsx
@@ -1,18 +1,18 @@
 function ResumePanel() {
   return (
     <div>
-      <div id="gallery02" className="gallery-component instance-2">
+      <div id="Profile_secondary" className="gallery-component instance-2">
         <div className="inner">
           <ul>
             <li>
-              <a href="/assets/images/gallery02/a3df0269_original.jpg" className="thumbnail n01" onContextMenu={(e) => e.preventDefault()} onDragStart={(e) => e.preventDefault()} onAuxClick={(e) => e.preventDefault()}><span className="frame"><img src="/assets/images/gallery02/a3df0269_original.jpg" alt="Untitled" title="Untitled" onContextMenu={(e) => e.preventDefault()} onDragStart={(e) => e.preventDefault()} onAuxClick={(e) => e.preventDefault()} /></span></a>
+              <a href="/assets/images/Profile_secondary/a3df0269_original.jpg" className="thumbnail n01" onContextMenu={(e) => e.preventDefault()} onDragStart={(e) => e.preventDefault()} onAuxClick={(e) => e.preventDefault()}><span className="frame"><img src="/assets/images/Profile_secondary/a3df0269_original.jpg" alt="Untitled" title="Untitled" onContextMenu={(e) => e.preventDefault()} onDragStart={(e) => e.preventDefault()} onAuxClick={(e) => e.preventDefault()} /></span></a>
             </li>
           </ul>
         </div>
       </div>
       <ul id="buttons01" className="buttons-component instance-1">
         <li>
-          <a href="#" className="n01" role="button"><svg aria-labelledby="buttons01-icon-1-title"><title id="buttons01-icon-1-title">PDF Document</title><use href="/assets/icons.svg#pdf"></use></svg><span className="label">Download My Resume</span></a>
+          <a href="#" className="n01" role="button" target="_blank" rel="noreferrer"><svg aria-labelledby="buttons01-icon-1-title"><title id="buttons01-icon-1-title">PDF Document</title><use href="/assets/icons.svg#pdf"></use></svg><span className="label">Download My Resume</span></a>
         </li>
       </ul>
       <div className="text-component instance-11">
