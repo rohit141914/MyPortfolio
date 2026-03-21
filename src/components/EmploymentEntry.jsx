cf182dbe9d7b3dfb0d2f0583d6253bb2eef5c7ba
rohit141914 <rohitnainindian@gmail.com>
2026-03-21 17:41:26 +0530

    removed ES and kr language code

--- src/components/EmploymentEntry.jsx
+++
@@ -1,9 +0,0 @@
-function EmploymentEntry({ dateRange, studioName, position, location, website }) {
-  return (
-    <span className="p">
-      <code>{dateRange}</code><br /><u>{studioName}</u><br /><strong>{position}</strong><br />{location}<br />Website: <a href={website} target="_blank" rel="noreferrer">{website}</a>
-    </span>
-  )
-}
-
-export default EmploymentEntry
